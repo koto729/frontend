080b6d7ca9c0f548f1aa850018e5ede167034cb9
koto729 <82299926+koto729@users.noreply.github.com>
2023-06-12 19:07:22 +0000

    add filter

--- src/Cat.jsx
+++ src/Cat.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row, Form } from 'react-bootstrap';
 import Header from './Header/header';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function Cat() {
   const [cats, setCats] = useState([]);
+  const [breedFilter, setBreedFilter] = useState('');
+  const [centerFilter, setCenterFilter] = useState('');
 
   useEffect(() => {
     const fetchCats = async () => {
       try {
-        const response = await axios.get('https://backend.koto123.repl.co/api/cats');
+        let url = 'https://backend.koto123.repl.co/api/cats';
+
+        // Add breed filter if provided
+        if (breedFilter) {
+          url += `/breed/${breedFilter}`;
+        }
+
+        // Add center filter if provided
+        if (centerFilter) {
+          url += `/center/${centerFilter}`;
+        }
+
+        const response = await axios.get(url);
         setCats(response.data);
       } catch (error) {
         console.log(error);
@@ -19,11 +33,19 @@ function Cat() {
     };
 
     fetchCats();
-  }, []);
+  }, [breedFilter, centerFilter]);
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString();
+  };
+
+  const handleBreedChange = (event) => {
+    setBreedFilter(event.target.value);
+  };
+
+  const handleCenterChange = (event) => {
+    setCenterFilter(event.target.value);
   };
 
   
@@ -31,6 +53,28 @@ function Cat() {
     <Container>
     <Header />
       <h2>Animals for Adoption</h2>
+      <Form>
+      <Row>
+        <Col>
+          <Form.Group controlId="breedFilter">
+            <Form.Label>Breed:</Form.Label>
+            <Form.Control type="text" value={breedFilter} onChange={handleBreedChange} />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId="centerFilter">
+            <Form.Label>Center:</Form.Label>
+            <Form.Control as="select" value={centerFilter} onChange={handleCenterChange}>
+              <option value="">All Centers</option>
+              <option value="HKCentre">Hong Kong Center</option>
+              <option value="Example Centre">Kowloon Center</option>
+              <option value="Sha Tin Center">Sha Tin Center</option>
+            </Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
+      </Form>
+      <br/>
       <Row>
         {cats.map((cat) => (
           <Col key={cat._id} md={4} className="mb-4">
