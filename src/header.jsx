a205a5a60ab2977594c7073b980700dad01962bb
koto729 <82299926+koto729@users.noreply.github.com>
2023-06-12 13:27:58 +0000

    login&show cat ui

---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* Your header content */}
+      <h1>My App</h1>
+      {/* ... */}
+    </header>
+  );
+}
+
+export default Header;
